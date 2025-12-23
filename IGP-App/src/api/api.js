bfc73e4719bbf62dbaf5ae678270c717c871cbd1
Epicmanpreet01 <manpreet210028@gmail.com>
2025-12-23 18:00:29 +0530

    fixed issues

--- IGP-App/src/api/api.js
+++ IGP-App/src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
-export const API_BASE =
-  process.env.EXPO_PUBLIC_API_BASE_URL || "http://192.168.1.4:5000/api";
+export const API_BASE = process.env.EXPO_PUBLIC_API_BASE_URL;
 
 console.log(API_BASE);
 
